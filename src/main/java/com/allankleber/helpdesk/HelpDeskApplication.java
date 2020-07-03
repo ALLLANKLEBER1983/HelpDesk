cd74beaa4cb84d6417fbaf2b072b732075caa66d
allan.kleber <allan.kleber@esig.com.br>
2020-07-03 00:00:13 -0300

    correção

--- src/main/java/com/allankleber/helpdesk/HelpDeskApplication.java
+++ src/main/java/com/allankleber/helpdesk/HelpDeskApplication.java
@@ -1,7 +1,14 @@
 package com.allankleber.helpdesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.allankleber.helpdesk.api.entity.User;
+import com.allankleber.helpdesk.api.enums.ProfileEnum;
+import com.allankleber.helpdesk.api.repository.UserRepository;
 
 @SpringBootApplication
 public class HelpDeskApplication {
@@ -9,5 +16,25 @@ public class HelpDeskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskApplication.class, args);
 	}
+	
+    @Bean
+    CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            initUsers(userRepository, passwordEncoder);
+        };
+
+    }
+    
+	private void initUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        User admin = new User();
+        admin.setEmail("admin@helpdesk.com");
+        admin.setPassword(passwordEncoder.encode("123456"));
+        admin.setProfile(ProfileEnum.ROLE_ADMIN);
+
+        User find = userRepository.findByEmail("admin@helpdesk.com");
+        if (find == null) {
+            userRepository.save(admin);
+        }
+    }
 
 }
